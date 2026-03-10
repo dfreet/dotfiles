db230ab26194cb3e75b64985e707e1ca46036b09
Devyn Freet <devyn@devynfreet.com>
2026-03-10 15:08:01 -0400

    set up home manager

--- flake.nix
+++ flake.nix
@@ -4,19 +4,29 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-25.11";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, nixos-hardware }:
     let
+      system = "x86_64-linux";
       lib = nixpkgs.lib;
+      pkgs = nixpkgs.legacyPackages.${system};
     in {
       nixosConfigurations = {
         nixlegion = lib.nixosSystem {
-          system = "x86_64-linux";
+          inherit system
 	  modules = [ 
 	    ./configuration.nix
 	    nixos-hardware.nixosModules.lenovo-legion-16iax10h
 	  ];
+	};
+      };
+      homeConfigurations = {
+        devyn = home-manager.lib.homeManagerConfiguration {
+	  inherit pkgs
+	  modules = [ ./home.nix ];
 	};
       };
     };
